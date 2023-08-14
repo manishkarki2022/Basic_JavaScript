0b93279de6972061050a462a2b907b81cd449228
Manish <96905327+manishkarki2022@users.noreply.github.com>
2023-08-14 07:27:04 +0000

    object Part1

---
+++ basic_02/object_Part1.js
@@ -0,0 +1,22 @@
+// // There are two type of Object:-
+// 1. SingleTone
+// which is Create By
+// Object.create// this is called contructer method 
+
+//object literals
+const jsUser={
+    name:"Manish",
+    age:18,
+    location:"Kathmandu",
+    email:"manish@google.com",
+    isLogedIn:false,
+    lastLogedDays:["Sumday","Monday"]
+}
+// There are different Way to access the value of Object
+
+// console.log(jsUser.name)
+// output:Manish
+
+ // The best way to access object value
+// console.log(jsUser["name"])
+// // output:Manish
