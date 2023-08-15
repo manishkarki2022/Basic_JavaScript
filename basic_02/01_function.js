3aff44a376305a1e6eb4cda5c6713f899e7581f8
Manish <96905327+manishkarki2022@users.noreply.github.com>
2023-08-15 11:47:12 +0000

    function

---
+++ basic_02/01_function.js
@@ -0,0 +1,20 @@
+// function addTwoNumber(...num1){
+//     let size = num1.length
+//     let total =0;
+//     for(let i =0;i<size;i++){
+//         total +=num1[i];
+//     }return total
+
+   
+    
+// }
+// console.log(addTwoNumber(1,1,1,1,1))
+
+
+// function loginUserMessage(username){
+//     if(!username){
+//         console.log("Please enter a Username")
+//     }
+//     return `${username} just logged in `
+// }
+// console.log(loginUserMessage("Manish"))
