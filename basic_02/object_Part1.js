75b56dff3b3b1811a3a8facfa5a13502b3dd4def
Manish <96905327+manishkarki2022@users.noreply.github.com>
2023-08-15 11:47:29 +0000

    Object part 1

--- basic_02/object_Part1.js
+++ basic_02/object_Part1.js
@@ -4,11 +4,14 @@
 // Object.create// this is called contructer method 
 
 //object literals
+const mySmb = Symbol("key1")
 const jsUser={
     name:"Manish",
+    [mySmb]:"mykey1",
     age:18,
     location:"Kathmandu",
     email:"manish@google.com",
+
     isLogedIn:false,
     lastLogedDays:["Sumday","Monday"]
 }
@@ -20,3 +23,20 @@ const jsUser={
  // The best way to access object value
 // console.log(jsUser["name"])
 // // output:Manish
+// console.log(jsUser.name)
+// console.log(jsUser['email'])
+// console.log(typeof jsUser[mySmb])
+jsUser.email="karki@google.com"
+// console.log(jsUser)
+Object.freeze(jsUser.email)
+jsUser.name ="Hero Manish"
+// console.log(jsUser)
+jsUser.greeting = function(){
+    console.log("Hello JS users")
+}
+jsUser.greetingTwo=function(){
+    console.log(`Hello JS User ${this.name}`)
+
+}
+console.log(jsUser.greeting())
+console.log(jsUser.greetingTwo())
