203ba221484227376212e656a02eabb3ebf4361c
Manish <96905327+manishkarki2022@users.noreply.github.com>
2023-08-16 10:51:34 +0000

    function Practise

---
+++ Practise/function.js
@@ -0,0 +1,18 @@
+//Write a JavaScript function that reverses a number.
+// function reverse(num){
+//     let number=num.toString()
+  
+//     let reverseNumber= number.split('').reverse().join('');
+   
+//      let Original=Number(reverseNumber,10)
+//     return Original
+
+// }
+// let number=32243
+// console.log("Original Number:", number)
+// console.log("Reserved Number:",reverse(number))
+//Output
+// Original Number: 32243
+// Reserved Number: 34223
+
+
