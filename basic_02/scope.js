785a7faf4a35f2ee26b85e8be98b5ec67050352d
Manish <96905327+manishkarki2022@users.noreply.github.com>
2023-08-16 14:56:44 +0000

    scope

---
+++ basic_02/scope.js
@@ -0,0 +1,19 @@
+
+// function myFunction(){
+//     let a = 10;
+//     const b = 20
+    
+// }
+// console.log(a)// a cannot be printed due to local Scope
+// console.log(b)// b cannot be printed due to local Scope
+
+// let a=10
+// const b=20;
+// function myFunction(){
+//     return a+b
+
+// }
+// console.log(myFunction())
+// Output: 30 // So a and b can be use in inside the function 
+//So it is called Gobal Scope, which is accessable through any were
+
