b8c874ffcae5a8b505479cc7421b38c0a70269bc
Manish <96905327+manishkarki2022@users.noreply.github.com>
2023-08-16 15:20:51 +0000

    scope update

--- basic_02/scope.js
+++ basic_02/scope.js
@@ -17,3 +17,24 @@
 // Output: 30 // So a and b can be use in inside the function 
 //So it is called Gobal Scope, which is accessable through any were
 
+
+// function one(){
+//     const username="Manish"
+//     function two(){
+//         const website="youtube"
+//         console.log(username)
+//     }
+//     two()
+    
+    
+// }
+// one()
+if(true){
+    const username = 'manish'
+    if(username=='manish'){
+        const website = " youtube"
+        console.log(username + website)
+    }
+    //console.log(website)
+}
+// console.log(username)
