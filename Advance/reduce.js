3bd5c9122ba1b92aa570ace05c404a0abd2bc6a5
Manish <96905327+manishkarki2022@users.noreply.github.com>
2023-08-19 06:15:46 +0000

    reduce

---
+++ Advance/reduce.js
@@ -0,0 +1,34 @@
+// const myNums=[1,2,3,4]
+// const initialValue=10
+
+
+// const myTotal = myNums.reduce((acc,curVal)=>{
+//     console.log(`acc:${acc} and curVal: ${curVal}`)
+  
+//     return acc +curVal
+
+// },initialValue)
+// console.log(myTotal)
+
+// const shoppingCart=[
+//     {
+//         itemName:"Js Course",
+//         price   :2999
+//     },
+//     {
+//         itemName:"Python Course",
+//         price   :999
+//     },
+//     {
+//         itemName:"Mobile Development Course",
+//         price   :5999
+//     },
+//     {
+//         itemName:"Data Science Course",
+//         price   :12999
+//     },
+// ]
+// const total= shoppingCart.reduce((acc,item)=>{
+//     return acc+item.price;
+// },0)
+// console.log(`Total: ${total}`)
